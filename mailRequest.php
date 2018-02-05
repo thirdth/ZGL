bf0c08593102f609228e5c9b683ac981b4e7ae40
Zack Glaser <zackglaser@gmail.com>
2018-02-05 16:52:34 -0600

    fused with Page template

--- mailRequest.php
+++ mailRequest.php
@@ -15,93 +15,129 @@ $from = $email;
 $message = "Name: " . $fName . " " . $lName . "\r\n Business: " . $busName . "\r\n Street: " . $street . "\r\n City, ST, Zip: " . $city . ", " . $state . ", " . $zip . "\r\n Comments: " . $comments;
 
 
- $is_not_home = ( 'page' == get_option( 'show_on_front' ) && ( '' != get_option( 'page_for_posts' ) ) && $wp_query->get_queried_object_id() == get_option( 'page_for_posts' ) );
-
- if ( $is_not_home ) :
-  get_header();
-
- $left_sidebar   = onetone_option('left_sidebar_blog_archive','');
- $right_sidebar  = onetone_option('right_sidebar_blog_archive','');
- $aside          = 'no-aside';
- if( $left_sidebar !='' )
- $aside          = 'left-aside';
- if( $right_sidebar !='' )
- $aside          = 'right-aside';
- if(  $left_sidebar !='' && $right_sidebar !='' )
- $aside          = 'both-aside';
-
- ?>
-   <section class="page-title-bar title-left no-subtitle">
-     <div class="container">
-       <hgroup class="page-title">
-         <h1>
-         <?php
- 		 $blog_title = get_the_title( get_option('page_for_posts', true) );
- 		 echo $blog_title;
- 		?>
-         </h1>
-       </hgroup>
 
 
 
-  <?php onetone_get_breadcrumb(array("before"=>"<div class=''>","after"=>"</div>","show_browse"=>false,"separator"=>'','container'=>'div'));?>
-       <div class="clearfix"></div>
-     </div>
-   </section>
- <div class="post-wrap">
-             <div class="container">
-                 <div class="post-inner row <?php echo $aside; ?>">
-                     <div class="col-main">
-                         <section class="post-main" role="main" id="content">
-                             <article class="page type-page" role="article">
-                               <?php
-                               if (mail ($to , $subject , $message, $from, '-fzack@zackglaserlegal.com')) {
-                                         echo "<div class='col-md-12 text-center'>
-                                                 <h3>Thank you for your interest. Your request was sent successfully. Our office will contact you shortly to set-up a consultation.</h3>
-                                                  </div>";
-                                       } else {
-                                         echo "<div class='col-md-12 text-center'>Something went wrong. Please send your request again. If you continue to have trouble, please contact our office directly.</div>";
-                                       };?>
-                             <?php if (have_posts()) :?>
-                                 <!--blog list begin-->
-                                 <div class="blog-list-wrap">
+get_header();
+$display_breadcrumb = 'yes';
 
-                                 <?php while ( have_posts() ) : the_post();?>
-                                 <?php get_template_part("content",get_post_format()); ?>
-                                 <?php endwhile;?>
-                                 </div>
-                                 <?php endif;?>
-                                 <!--blog list end-->
-                                 <!--list pagination begin-->
-                                 <nav class="post-list-pagination" role="navigation">
-                                     <?php if(function_exists("onetone_native_pagenavi")){onetone_native_pagenavi("echo",$wp_query);}?>
-                                 </nav>
-                                 <!--list pagination end-->
-                             </article>
-                             <div class="post-attributes"></div>
-                         </section>
-                     </div>
-                     <?php if( $left_sidebar !='' ):?>
-                     <div class="col-aside-left">
-                         <aside class="blog-side left text-left">
-                             <div class="widget-area">
-                                 <?php get_sidebar('archiveleft');?>
-                             </div>
-                         </aside>
-                     </div>
-                     <?php endif; ?>
-                     <?php if( $right_sidebar !='' ):?>
-                     <div class="col-aside-right">
-                        <?php get_sidebar('archiveright');?>
-                     </div>
-                     <?php endif; ?>
 
-                 </div>
-             </div>
-         </div>
- <?php get_footer();?>
- <?php
-   else:
- ?>
- <?php get_template_part('template','home');?>
- <?php endif;?>
+$sidebar                   = isset($page_meta['page_layout'])?$page_meta['page_layout']:'none';
+$left_sidebar              = isset($page_meta['left_sidebar'])?$page_meta['left_sidebar']:'';
+$right_sidebar             = isset($page_meta['right_sidebar'])?$page_meta['right_sidebar']:'';
+$full_width                = isset($page_meta['full_width'])?$page_meta['full_width']:'no';
+$display_breadcrumb        = isset($page_meta['display_breadcrumb'])?$page_meta['display_breadcrumb']:$display_breadcrumb;
+$display_title             = isset($page_meta['display_title'])?$page_meta['display_title']:'yes';
+$padding_top               = isset($page_meta['padding_top'])?$page_meta['padding_top']:'';
+$padding_bottom            = isset($page_meta['padding_bottom'])?$page_meta['padding_bottom']:'';
+
+if( $full_width  == 'no' )
+ $container = 'container';
+else
+ $container = 'container-fullwidth';
+
+$aside          = 'no-aside';
+if( $sidebar =='left' )
+$aside          = 'left-aside';
+if( $sidebar =='right' )
+$aside          = 'right-aside';
+if(  $sidebar =='both' )
+$aside          = 'both-aside';
+
+$container_css = '';
+if( $padding_top )
+$container_css .= 'padding-top:'.$padding_top.';';
+if( $padding_bottom )
+$container_css .= 'padding-bottom:'.$padding_bottom.';';
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+
+ <?php if (  $display_breadcrumb == 'yes' ): ?>
+
+
+  <section class="page-title-bar title-left no-subtitle" style="">
+    <div class="container">
+      <hgroup class="page-title">
+        <h1>
+          <?php the_title();?>
+        </h1>
+      </hgroup>
+
+
+   <?php onetone_get_breadcrumb(array("before"=>"<div class=''>","after"=>"</div>","show_browse"=>false,"separator"=>'','container'=>'div'));?>
+
+      <div class="clearfix"></div>
+    </div>
+  </section>
+
+  <?php endif;?>
+
+  <div class="post-wrap">
+    <div class="<?php echo $container;?>">
+      <div class="post-inner row <?php echo $aside; ?>" style=" <?php echo $container_css;?>">
+        <div class="col-main">
+          <section class="post-main" role="main" id="content">
+            <?php while ( have_posts() ) : the_post(); ?>
+            <article class="post type-post" role="article">
+              <?php if (  has_post_thumbnail() ): ?>
+              <div class="feature-img-box">
+                <div class="img-box">
+                  <?php the_post_thumbnail();?>
+                </div>
+              </div>
+              <?php endif;?>
+              <div class="entry-main">
+
+                <div class="entry-content">
+                  <?php
+                  if (mail ($to , $subject , $message, $from, '-fzack@zackglaserlegal.com')) {
+                            echo "<div class='col-md-12 text-center'>
+                                    <h3>Thank you for your interest. Your request was sent successfully. Our office will contact you shortly to set-up a consultation.</h3>
+                                     </div>";
+                          } else {
+                            echo "<div class='col-md-12 text-center'>Something went wrong. Please send your request again. If you continue to have trouble, please contact our office directly.</div>";
+                          };
+                  ?>
+                  <?php the_content();?>
+                  <?php
+				wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'onetone' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) );
+				?>
+                </div>
+
+              </div>
+            </article>
+            <div class="post-attributes">
+              <!--Comments Area-->
+              <div class="comments-area text-left">
+                <?php
+					  // If comments are open or we have at least one comment, load up the comment template
+					  if ( comments_open()  ) :
+						  comments_template();
+					  endif;
+				  ?>
+              </div>
+              <!--Comments End-->
+            </div>
+            <?php endwhile; // end of the loop. ?>
+          </section>
+        </div>
+        <?php if(  $sidebar =='left' || $sidebar =='both' ):?>
+        <div class="col-aside-left">
+          <aside class="blog-side left text-left">
+            <div class="widget-area">
+              <?php get_sidebar('pageleft');?>
+            </div>
+          </aside>
+        </div>
+        <?php endif; ?>
+        <?php if(  $sidebar =='right' || $sidebar =='both' ):?>
+        <div class="col-aside-right">
+          <?php get_sidebar('pageright');?>
+        </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+</article>
+<?php get_footer();
