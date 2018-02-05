f9a32f423ed64b8f2cb5efb5c72d6586579e1cb8
Zack Glaser <zackglaser@gmail.com>
2018-02-05 16:04:30 -0600

    debugging

--- mailRequest.php
+++ mailRequest.php
@@ -12,15 +12,20 @@ $comments = filter_var($_POST['comments'], FILTER_UNSAFE_RAW, FILTER_FLAG_HIGH);
 $to = 'zack@zackglaserlegal.com';
 $subject = 'Zack Glaser Legal Request';
 $message = "Name: " . $fName . " " . $lName . "\r\n Business: " . $busName . "\r\n Street: " . $street . "\r\n City, ST, Zip: " . $city . ", " . $state . ", " . $zip . "\r\n Comments: " . $comments;
+
+
+echo $fName . ", " . $lName . ", " . $busName . ", " . $email;
+/*
 if (mail ($to , $subject , $message, $from, '-fzack@zackglaserlegal.com')) {
           echo "<div class='col-md-12 text-center'>
                   <h3>Your message has been sent. </h3>
                    </div>";
         } else {
           echo "<div class='col-md-12 text-center'>Something went wrong. Please send your request again. If you continue to have trouble, please contact our office directly.</div>";
-        }
+        };
         echo '<pre>';
         print_r($debug_array);
         echo '</pre>';
 
  ?>
+ */
